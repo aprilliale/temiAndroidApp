739a048b3f1417ee3ad717ebfba68f528049957f
Erron Lolha Aprillia <aprilliale@hanyang.ac.kr>
2022-11-23 21:57:06 +0900

    second commit

--- app/src/main/java/com/example/temiandroidapp/Splashscreen.java
+++ app/src/main/java/com/example/temiandroidapp/Splashscreen.java
@@ -2,18 +2,25 @@ package com.example.temiandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+
+public class Splashscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        setContentView(R.layout.activity_layout);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                Intent i = new Intent(Splashscreen.this, MainActivity.class); startActivity(i);
+                finish(); } }, 3000);
+
     }
 
     @Override
@@ -30,4 +37,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
