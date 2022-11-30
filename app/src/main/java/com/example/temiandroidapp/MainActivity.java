b28ed7cdd732056708330d2b0cea4520e4d3b4d3
Erron Lolha Aprillia <aprilliale@hanyang.ac.kr>
2022-11-30 21:36:19 +0900

    main activity buttons commit

--- app/src/main/java/com/example/temiandroidapp/MainActivity.java
+++ app/src/main/java/com/example/temiandroidapp/MainActivity.java
@@ -1,19 +1,75 @@
 package com.example.temiandroidapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton startButton, settingButton,exitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(R.layout.activity_layout);
+
+        addListenerOnButton();
+    }
+
+    // BUTTON CODE
+    private void addListenerOnButton() {
+
+        startButton = (ImageButton) findViewById(R.id.startButton);
+        settingButton = (ImageButton) findViewById(R.id.settingButton);
+        exitButton = (ImageButton) findViewById(R.id.exitButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+            }
+        });
+
+        settingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "this is setting", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setMessage("Do you want to exit?").setCancelable(false).
+                        setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                MainActivity.super.onBackPressed();
+                            }
+                        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 
     @Override
