9802746dd2c09daf8874f67d97d5885252d31f2e
Erron Lolha Aprillia <aprilliale@hanyang.ac.kr>
2022-12-23 20:35:46 +0900

    Layout fixing

--- app/src/main/java/com/example/temiandroidapp/playingRoom.java
+++ app/src/main/java/com/example/temiandroidapp/playingRoom.java
@@ -4,15 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class playingRoom extends AppCompatActivity {
+
+    ImageButton buttonUp, buttonDown, buttonLeft, buttonRight, buttonForward, buttonBackward, buttonShot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing_room);
+
+        //button code
+        upListener();
+        downListener();
+        leftListener();
+        rightListener();
+        forwardListener();
+        backwardListener();
+        shotListener();
     }
 
+    private void upListener() {
+        buttonUp = findViewById(R.id.buttonUP);
+    }
+
+    private void downListener() {
+        buttonDown = findViewById(R.id.buttonDown);
+
+    }
+
+    private void leftListener() {
+        buttonLeft = findViewById(R.id.buttonLeft);
+    }
+
+    private void rightListener() {
+        buttonRight = findViewById(R.id.buttonRight);
+
+    }
+
+    private void forwardListener() {
+        buttonForward = findViewById(R.id.buttonForward);
+
+    }
+
+    private void backwardListener() {
+        buttonBackward = findViewById(R.id.buttonBackward);
+    }
+
+    private void shotListener() {
+        buttonShot = findViewById(R.id.buttonShot);
+    }
 
 
     @Override
